6b28f44a98ef8bc88f3ebad1657dacd50747daac
Daniel Martins <danielmartins10@gmail.com>
2021-03-26 19:26:23 -0300

    componentização da aplicaçao

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
+import { useContext  } from "react";
+import { Button } from "../components/Button";
+import { MoviesContext } from "../hooks/useMoviesContext";
+
 export function SideBar() {
-  // Complete aqui
+  const { genres, selectedGenreId, setSelectedGenreId } = useContext(MoviesContext);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,8 @@ module.exports = {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
   devServer: {
+    host: "0.0.0.0",
+    port: 8080,
     contentBase: path.resolve(__dirname, 'public'),
     hot: true,
   },
